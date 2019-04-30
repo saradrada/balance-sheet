121ddba9807f76a7eb3fa5bfacb94dadacf90fd3
saradrada <saradrada@hotmail.com>
2019-04-30 09:53:54 -0500

    Added project

---
+++ src/model/BalanceGeneral.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.ArrayList;
+
+public class BalanceGeneral {
+	
+	private double totalACorrientes;
+	private double totalAFijos;
+	private double totalActivos;
+	
+	private double totalPCorrientes;
+	private double totalPFijos;
+	private double totalPasivos;
+	
+	private double totalPatrimonio;
+	
+	private ArrayList<Dato> datos;
+	
+}
