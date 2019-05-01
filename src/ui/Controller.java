6eb201f8d7a40a84623ee7b77f6bf378d1d408a6
unknown <themonstermora@gmail.com>
2019-04-30 21:43:49 -0500

    Cambio en la manera de pedir los datos a agregar, se los explico por el grupo de wpp

--- src/ui/Controller.java
+++ src/ui/Controller.java
@@ -1,27 +1,26 @@
 package ui;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Optional;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
+import javafx.util.Pair;
 import model.Dato;
 
 public class Controller {
-
-	@FXML private Label labCoName;
-	@FXML private Label labDate;
 	
 	@FXML private ListView<String> listActivosCorrientes;
 	@FXML private ListView<String> listActivoFijos;
@@ -29,35 +28,53 @@ public class Controller {
 	@FXML private ListView<String> listPasivos;
 	@FXML private ListView<String> listIngresos;
 	@FXML private ListView<String> listPatrimonio;
-	
-	// Data
-	@FXML private Button btnCancelar;
 
-	@FXML private TextField txtNombreDato;
-	@FXML private TextField txtCantidadDato;
-
-	@FXML private ChoiceBox<String> cbTipoDato;
-	
-	@FXML
-	void agregarDatos(ActionEvent event) {
-		Parent root;
-		try {
-			root = FXMLLoader.load(getClass().getResource("data.fxml"));
-			Stage stage = new Stage();
-			stage.setScene(new Scene(root));
-			stage.show();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+	public void agregarDatos() {
+		GridPane grid=new GridPane();
+		Dialog<Pair<String, String>> dialog=new Dialog<>();
+		ButtonType loginButtonType = new ButtonType("Aceptar", ButtonData.OK_DONE);
+		dialog.getDialogPane().getButtonTypes().addAll(loginButtonType, ButtonType.CANCEL);	
+		dialog.setTitle("Agregar nueva cuenta");
+		dialog.setHeaderText(null);
+//		dialog.setGraphic(new ImageView(""));
+		TextField nombre=new TextField();
+		nombre.setPromptText("Nombre de la cuenta");
+		TextField valor=new TextField();
+		valor.setPromptText("Valor de la cuenta");
+		ChoiceBox<String> choices=new ChoiceBox<>();
+		choices.setItems(FXCollections.observableArrayList("Activo corriente", "Activo no corriente", "Pasivo",
+				"Patrimonio", "Ingreso", "Gasto"));
+		grid.add(new Label("Nombre de la cuenta: "), 0, 0);
+		grid.add(nombre, 1, 0);
+		grid.add(new Label("Valor de la cuenta: "), 0, 1);
+		grid.add(valor, 1, 1);
+		grid.add(new Label("Seleccione el tipo: "), 0, 2);
+		grid.add(choices, 1, 2);
+		dialog.getDialogPane().setContent(grid);
+		dialog.setResultConverter(f -> {
+		    if (f == loginButtonType) {
+		        return new Pair<>(nombre.getText(), valor.getText());
+		    }
+		    return null;
+		});
+		Optional<Pair<String, String>> result = dialog.showAndWait();
+		result.ifPresent(e -> {
+		    aja(e.getKey(), e.getValue(), choices.getValue());
+		    System.out.println(e.getKey()+" : "+e.getValue()+" : "+choices.getValue());
+		});
 	}
-
+	
+	public void aja(String nombre, String valor, String tipo) {
+		Main.getBalanceGeneral().addDato(nombre, Double.parseDouble(valor), tipo);
+	}
+	
 	public void actualizarListas(String tipo) {
 		ObservableList<String> a=FXCollections.observableArrayList();
 		ArrayList<String> b=Main.getBalanceGeneral().getPorTipo(tipo);
 		for(int i=0;i<b.size();i++) {
 			a.add(b.get(i));
 		}
+		System.out.println(a.get(0));
 		if(tipo.equalsIgnoreCase(Dato.ACTIVO_CORRIENTE)) {
 			listActivosCorrientes.setItems(a);
 		}else if(tipo.equalsIgnoreCase(Dato.ACTIVO_NO_CORRIENTE)) {
@@ -71,31 +88,6 @@ public class Controller {
 		}else if(tipo.equalsIgnoreCase(Dato.PATRIMONIO)) {
 			listPatrimonio.setItems(a);
 		}
-	}
-	
-	@FXML
-	void agregarDato(ActionEvent event) {
-		try {
-			String nombreDato = txtNombreDato.getText();
-			String tipo=cbTipoDato.getValue();
-			double cantidadDato = Double.parseDouble(txtCantidadDato.getText());
-			Main.getBalanceGeneral().addDato(nombreDato, cantidadDato, tipo);
-			actualizarListas(tipo);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
-	}
-
-	@FXML
-	void cancelarDato(ActionEvent event) {
-		((Stage) (((Button) event.getSource()).getScene().getWindow())).close();
-	}
-
-	@FXML
-	void setChoiceBox() {
-		cbTipoDato.setItems(FXCollections.observableArrayList("Activo corriente", "Activo no corriente", "Pasivo",
-				"Patrimonio", "Ingreso", "Gasto"));
 	}
 
 }
